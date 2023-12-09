33eac2a4fa0fe7916529ffe76d100a992e47e9ad
TamaraRobson <tamara.jade.robson@gmail.com>
2023-12-09 16:03:27 +0100

    Filter getAllReports

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,23 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { ReportType, data } from './data';
 
 @Controller('report/:type')
 export class AppController {
   @Get()
-  getAllIncomeReports() {
-    return [];
+  getAlReports(@Param('type') type: string) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return data.report.filter((report) => report.type === reportType);
   }
 
   @Get(':id')
-  getIncomeReportById() {
-    return {};
+  getIncomeReportById(@Param('type') type: string, @Param('id') id: string) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+
+    return data.report
+      .filter((report) => report.type === reportType)
+      .find((report) => report.id === id);
   }
 
   @Post()
