b0d95e2d228f8f20a69678509cf61e4a8c933ba6
TamaraRobson <tamara.jade.robson@gmail.com>
2023-12-11 11:20:09 +0100

    Transform propety created_at into createdAt

--- src/dto/report.dto.ts
+++ src/dto/report.dto.ts
@@ -5,7 +5,7 @@ import {
   IsNotEmpty,
   IsOptional,
 } from 'class-validator';
-import { Exclude } from 'class-transformer';
+import { Exclude, Expose } from 'class-transformer';
 import { ReportType } from 'src/data';
 
 export class CreateReportDto {
@@ -34,6 +34,13 @@ export class ReportResponseDto {
   id: string;
   source: string;
   amount: number;
+
+  @Expose({ name: 'createdAt' })
+  transformCreatedAt() {
+    return this.created_at;
+  }
+
+  @Exclude()
   created_at: Date;
 
   @Exclude()
